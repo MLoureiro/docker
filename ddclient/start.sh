bd69a4a0ef389c6ebb6f0d4de99215c734a36a2a
Marcos Loureiro <mloureiro.dev@gmail.com>
2016-01-12 11:15:53 +0000

    Fix paths variables

--- ddclient/start.sh
+++ ddclient/start.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 DEFAULT_CONF_PATH=/root/ddclient/ddclient.conf
-CONF_PATH=/config/ddclient.conf
+CUSTOM_CONF_PATH=/config/ddclient.conf
 
 # Search for custom config file, if it doesn't exist, copy the default one
 if [ ! -f /config/ddclient.conf ]; then
   echo "Creating config file. Please do not forget to enter your info in ddclient.conf."
-  cp $DEFAULT_CONF_PATH $CONF_PATH
-  chmod a+w $CONF_PATH
+  cp $DEFAULT_CONF_PATH $CUSTOM_CONF_PATH
+  chmod a+w $CUSTOM_CONF_PATH
   exit 1
 fi
 
-if [[ `diff $DEFAULT_CONF_PATH $CONF_PATH` == "" ]]
-then
+# Verify if the configuration file was changed or not
+if [[ `diff $DEFAULT_CONF_PATH $CUSTOM_CONF_PATH` == "" ]]; then
   echo "Please update the config file with your required data"
   exit 1
 fi
-
